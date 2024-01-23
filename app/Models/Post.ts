a3d7466ec62f576fc1094306f9d58482ba11bf1c
wesleyfsousa01 <wesleyfsouza10@gmail.com>
2024-01-23 17:13:38 -0300

    Modelos e Migrations: Post, Comment criados

---
+++ app/Models/Post.ts
@@ -0,0 +1,23 @@
+import { DateTime } from 'luxon'
+import { BaseModel, HasMany, column, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import Comment from './Comment'
+
+export default class Post extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public title: string
+
+  @column()
+  public content: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @hasMany(() => Comment)
+  public comments: HasMany<typeof Comment>
+}
